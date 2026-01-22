c52d6be3d9b6ca5a859c282b5290e30cf2576177
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-22 14:00:38 +0000

    rmv tp flag, add next config, add dist to gitignore

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  basePath: isProd ? "/" : "",
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
