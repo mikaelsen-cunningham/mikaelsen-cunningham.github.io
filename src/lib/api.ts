4fc25cff996997ba1cabc3f149e1b60bc66c73a1
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-22 18:19:44 +0000

    post to projects name,

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,26 +3,26 @@ import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
-const postsDirectory = join(process.cwd(), "_posts");
+const projectsDirectory = join(process.cwd(), "_site-content", "projects");
 
-export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+export function getProjectSlugs() {
+  return fs.readdirSync(projectsDirectory);
 }
 
-export function getPostBySlug(slug: string) {
+export function getProjectBySlug(slug: string) {
   const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = join(postsDirectory, `${realSlug}.md`);
+  const fullPath = join(projectsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
   return { ...data, slug: realSlug, content } as Post;
 }
 
-export function getAllPosts(): Post[] {
-  const slugs = getPostSlugs();
-  const posts = slugs
-    .map((slug) => getPostBySlug(slug))
-    // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
-  return posts;
+export function getAllProjects(): Post[] {
+  const slugs = getProjectSlugs();
+  const projects = slugs
+    .map((slug) => getProjectBySlug(slug))
+    // sort projects by date in descending order
+    .sort((project1, project2) => (project1.date > project2.date ? -1 : 1));
+  return projects;
 }
