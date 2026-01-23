cccd217edeb7ed81705507b0de7f306ff06a8543
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-23 09:32:39 +0000

    init about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import Container from "../_components/container";
+
+const About = () => {
+  return (
+    <Container>
+      <h2 className="text-2xl font-bold mb-4">About Me</h2>
+      <p className="mb-2">
+        Hello! I'm Robin Cunningham, a passionate software developer with a love
+        for creating innovative solutions. With a background in computer
+        science, I enjoy tackling complex problems and turning ideas into
+        reality through code.{" "}
+      </p>
+    </Container>
+  );
+};
+
+export default About;
