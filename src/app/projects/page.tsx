48ee85e3abfd013796f31ec41b2bac4cd7aca1c0
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-27 16:07:25 +0000

    imp icon link list

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,17 +1,20 @@
 import Container from "@/app/_components/container";
 import PageLayout from "@/app/_components/page-layout";
+import { getAllProjects } from "@/lib/api";
+import IconLinkList from "@/app/_components/icon-link-list";
+import { NavItem } from "@/interfaces/navigation";
 
 const ProjectList = () => {
+  const projectPageNavItems: NavItem[] = getAllProjects().map((project) => ({
+    href: `/projects/${project.slug}`,
+    label: project.title,
+    hasChildren: false,
+  }));
+
   return (
     <PageLayout segments={["projects"]}>
       <Container>
-        <h1>Projects</h1>
-        <p>Welcome to the projects page. Here are some of my projects:</p>
-        <ul className="list-disc list-inside">
-          <li>Project Alpha</li>
-          <li>Project Beta</li>
-          <li>Project Gamma</li>
-        </ul>
+        <IconLinkList linkList={projectPageNavItems} />
       </Container>
     </PageLayout>
   );
