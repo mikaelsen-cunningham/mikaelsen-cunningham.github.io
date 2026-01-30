eccbef9986c1c9ab8223c864d09ce25f532aaa82
mikaelsen-cunningham <34577966+robinjmc@users.noreply.github.com>
2026-01-30 22:30:39 +0000

    render read me on homepage #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Container from "@/app/_components/container";
 import PageLayout from "@/app/_components/page-layout";
-import { Intro } from "@/app/_components/intro";
+import { getContentBySlug } from "@/lib/getPageDetail";
+import markdownToHtml from "@/lib/markdownToHtml";
 
-const Index = () => {
+const Index = async () => {
+  const readMeContent = getContentBySlug("readme", process.cwd());
+  const content = await markdownToHtml(readMeContent?.content || "");
   return (
     <PageLayout segments={[]}>
       <Container>
-        <Intro />
+        <div className="prose max-w-full mt-8">
+          <div dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
       </Container>
     </PageLayout>
   );
